7fa1ff8c721a8877a8834f1a0f8457cbb4ba7945
toma <tomajohn316@yahoo.com>
2022-08-19 14:28:35 +0100

    Create Database, Create table and Insert data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
+-- We will use an artificial PK
+CREATE TABLE animals(
+  id INT,
+  name VARCHAR(200),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  PRIMARY KEY(id)
 );
