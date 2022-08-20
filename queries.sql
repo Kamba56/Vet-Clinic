0f35b820c513e4304943954de4d85ecf365d8249
toma <tomajohn316@yahoo.com>
2022-08-20 09:22:47 +0100

    Query database and add screenshots

--- queries.sql
+++ queries.sql
@@ -12,4 +12,21 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = TRUE;SELECT * FROM animals WHERE neutered = TRUE;
 
- SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+SELECT COUNT(id) FROM animals;
+
+SELECT COUNT(id) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
